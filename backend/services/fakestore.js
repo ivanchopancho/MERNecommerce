b2146e4c063abfbc348ce46d7da57350ba8556a0
ivan <vivanchi@gmail.com>
2025-12-28 03:13:53 -0300

    Changing import syntax to Render-compatible

--- backend/services/fakestore.js
+++ backend/services/fakestore.js
@@ -1,7 +1,13 @@
-import products from "../data/products.json" assert { type: "json" };
+import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export async function fetchProducts() {
-    return products;
-}
-
+    const filePath = path.join(__dirname, "../data/products.json");
+    const file = await fs.readFile(filePath, "utf-8");
+    return JSON.parse(file);
+};
 
