0982269eab74a33206584db47b0ed33a8ee2c77c
ivan <vivanchi@gmail.com>
2025-12-28 03:00:09 -0300

    Fetching data as local due to invalid answers from fakestoreapi.com

--- backend/services/fakestore.js
+++ backend/services/fakestore.js
@@ -1,14 +1,7 @@
-import fetch from "node-fetch"
+import products from "../data/products.json" assert { type: "json" };
 
 export async function fetchProducts() {
-    const res = await fetch("https://fakestoreapi.com/products", {
-        headers: {
-            "User-Agent": "Mernecommerce/1.0 (learning project)",
-            "Accept": "application/json"
-        }
-    });
-    if (!res.ok) {
-        throw new Error(`FakeStore failed: ${res.status}`);
-    }
-    return res.json();
+    return products;
 }
+
+
