1210417a02890609086302ba40d9c4764cdde81d
ivan <vivanchi@gmail.com>
2025-12-28 01:54:09 -0300

    Wildcard fix on cors

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: "*"
+}));
 app.use(express.json());
 
 app.get("/health", (req, res) => {
